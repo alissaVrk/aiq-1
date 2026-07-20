85567c2d7473a6a68c157a24b4c490f26300cdf6
AlissaVrk <alissa@dazl.dev>
2026-07-20 11:27:43 +0300

    allowedDevOrigins

--- frontends/ui/next.config.ts
+++ frontends/ui/next.config.ts
@@ -7,6 +7,7 @@ const fileUploadMaxSizeMB = parseInt(process.env.FILE_UPLOAD_MAX_SIZE_MB || '100
 const inDazl = process.env.RUNNING_IN_DAZL === 'true'
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['*.dazl-preview.ninja', '*.infra-sandbox.dazl-preview.ninja'],
   typescript: {
     ignoreBuildErrors: true,
     ...(inDazl && { tsconfigPath: './tsconfig.dazl.json' }),
